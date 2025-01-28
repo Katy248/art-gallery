f148877bed3e3616028cab8808ce50e9687f9952
Katy248 <petrovanton247@gmail.com>
2025-01-28 11:25:49 +0300

    Rename some js

--- scripts/JavaSkripts4.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggle-sidebar').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
-});

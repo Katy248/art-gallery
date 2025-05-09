af9981c0d12b24695174fd3c767d03279290b792
Katy248 <petrovanton247@gmail.com>
2025-05-09 15:32:24 +0300

    Add image preview

--- client/src/views/CreatePostView.vue
+++ client/src/views/CreatePostView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { createPost } from "../api";
 import { useRouter } from "vue-router";
 
@@ -7,11 +7,17 @@ const router = useRouter();
 
 const description = ref("");
 const image = ref();
+const previewUrl = ref("");
 
 const onFileChange = (e) => {
     const file = e.target.files[0];
     console.log(file);
     image.value = file;
+    const fr = new FileReader();
+    fr.readAsDataURL(image.value);
+    fr.addEventListener("load", () => {
+        previewUrl.value = fr.result;
+    });
 };
 const onSubmit = () => {
     if (!image.value) {
@@ -36,12 +42,14 @@ const onSubmit = () => {
     <div>
         <form class="flex flex-col gap-4" @submit.prevent="onSubmit">
             <div class="input-group">
-                <label>Описание:</label>
                 <textarea v-model="description" class="text-entry" placeholder="Крутая фотка" />
             </div>
             <div class="input-group">
-                <label>Изображение:</label>
-                <input @change="onFileChange" accept="image/png, image/jpeg" type="file" class="text-entry" placeholder="example@mail.ru" autocomplete="username" />
+                <div v-if="image" class="w-full flex flex-col gap-4">
+                    <img :src="previewUrl" class="rounded-md" />
+                    <button class="btn btn-base" @click="image = null">Удалить фото</button>
+                </div>
+                <input v-if="!image" @change="onFileChange" accept="image/png, image/jpeg" type="file" class="text-entry" placeholder="example@mail.ru" autocomplete="username" />
             </div>
             <div class="input-group">
                 <input type="submit" class="btn btn-primary" />

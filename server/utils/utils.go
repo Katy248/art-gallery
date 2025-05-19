11be76a58c32be3c3ca9429060ec0d42863372c4
Katy248 <petrovanton247@gmail.com>
2025-05-19 15:11:57 +0300

    Delete validation

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -8,31 +8,12 @@ import (
 	"gorm.io/gorm"
 )
 
-type Validatable interface {
-	Validate() error
-}
-
 // Binds JSON to request pointer
 func BindRequest(r interface{}) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if err := ctx.BindJSON(&r); err != nil {
 			log.Errorf("Failed bind JSON data: %s", err)
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err})
-			return
-		}
-	}
-}
-
-func ValidateUri(r Validatable) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		if err := ctx.ShouldBindUri(&r); err != nil {
-			log.Errorf("Failed bind uri parameters: %s", err)
-			ctx.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-		if err := r.Validate(); err != nil {
-			log.Errorf("Validation failed: %s", err)
-			ctx.AbortWithError(http.StatusBadRequest, err)
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err})
 			return
 		}
 	}
@@ -43,7 +24,7 @@ func WrapDbResult(ctx *gin.Context, result *gorm.DB) bool {
 
 	if result.Error != nil {
 		log.Errorf("Failed perform database action: %s", result.Error)
-		ctx.JSON(http.StatusInternalServerError, gin.H{
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 			"status":  http.StatusInternalServerError,
 			"success": false,
 		})

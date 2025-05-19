18f6233cb0cbdb19bec40857f02cfabf5ed14aa4
Katy248 <petrovanton247@gmail.com>
2025-05-19 14:10:04 +0300

    Refactor main

--- server/main.go
+++ server/main.go
@@ -5,21 +5,14 @@ import (
 
 	db "art-gallery-server/database"
 	"art-gallery-server/database/migration"
-
-	"github.com/charmbracelet/log"
-	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 var (
-	port             = 8080
-	connectionString = "../art-gallery.db"
-	serverMode       = gin.ReleaseMode
+	port = 8080
 )
 
 func init() {
 	SetupConfiguration()
-	SetupLogging()
 }
 
 func main() {
@@ -31,20 +24,4 @@ func main() {
 	addr := fmt.Sprintf(":%d", port)
 	server := CreateServer(addr)
 	server.Run(addr)
-}
-
-func SetupLogging() {
-	level := viper.GetString("log.level")
-	switch level {
-	case "debug":
-		log.SetLevel(log.DebugLevel)
-	case "info":
-		log.SetLevel(log.InfoLevel)
-	case "warning":
-		log.SetLevel(log.WarnLevel)
-	case "error":
-		log.SetLevel(log.ErrorLevel)
-	}
-
-	log.Infof("Log level - %s", level)
 }

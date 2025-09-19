e53ae4a76ffeb6ea46efb28ffed672e063957cc1
Katy248 <petrovanton247@gmail.com>
2025-09-19 16:00:12 +0300

    Format code

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 
 if [[ $1 == "server" ]]; then
     cd /root/art-gallery/server || exit 2
-    
+
     export GOPATH='/root/go'
     export GOENV='/root/.config/go/env'
     export GOCACHE='/root/.cache/go-build'
     export GOMODCACHE='/root/go/pkg/mod'
     go run .
-    elif [[ $1 == "client" ]]; then
+elif [[ $1 == "client" ]]; then
     cd /root/art-gallery/client || exit 2
     npm run dev
 else

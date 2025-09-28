a2c89e304a0578db78b72b4baf5e7fe44a65c654
Katy248 <petrovanton247@gmail.com>
2025-09-28 17:29:33 +0300

    Small fix

--- server/endpoints/post/get/endpoint.go
+++ server/endpoints/post/get/endpoint.go
@@ -25,8 +25,8 @@ type Request struct {
 
 func handler(r *Request) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var post *shared.ResponsePost
-		database.Conn.Raw(rawSql, auth.UserId(c), r.PostId).First(post)
+		var post shared.ResponsePost
+		database.Conn.Raw(rawSql, auth.UserId(c), r.PostId).First(&post)
 
 		post.UpdateSavesCount()
 
